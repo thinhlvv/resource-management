e1806a1626276eb83dde620185fdca0e73faab72
Thinh Le <thinhle@Thinhs-MacBook-Pro.local>
2019-10-02 23:35:51 +0800

    separate migrations

--- migrations/20191002232659_init_resource_table.sql
+++ migrations/20191002232659_init_resource_table.sql
@@ -1,20 +1,20 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE TABLE IF NOT EXISTS user(
-  id INT UNSIGNED AUTO_INCREMENT,
-  quota INT,
-  email VARCHAR(255) UNIQUE NOT NULL,
-  hashed_password VARCHAR(255) NOT NULL,
-  role INT NOT NULL,
-  
+CREATE TABLE IF NOT EXISTS resource(
+	id INT UNSIGNED AUTO_INCREMENT,
+  user_id INT UNSIGNED NOT NULL,
+	name VARCHAR(255) UNIQUE NOT NULL,
+
   created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 	deleted_at DATETIME DEFAULT NULL,
-	PRIMARY KEY (id)
+	PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES user(id)
 ) ENGINE=InnoDB CHARACTER SET=utf8mb4 COLLATE=utf8mb4_general_ci;
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS resource;
 -- +goose StatementEnd
+
